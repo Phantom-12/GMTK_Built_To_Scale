01bfc237865f5d996357ae1b7965ea5fe8d2295c
Qiangzzz <1105796626@qq.com>
2024-09-20 21:42:44 +0800

    背景适配问题解决

--- Assets/Scripts/BGSelfAdaption.cs
+++ Assets/Scripts/BGSelfAdaption.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 
 public class BGSelfAdaption : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject Bg;
     // Start is called before the first frame update
     void Start()
     {
         float screenWidth = Screen.width;
         float screenHeight = Screen.height;
-        Bg.transform.localScale = new Vector3(MathF.Round(screenWidth/1920f,3), MathF.Round(screenHeight /1080f,3), 1);
+        gameObject.transform.localScale = new Vector3(MathF.Round((screenWidth/screenHeight)/(1920f/1080f),3),1, 1);
     }
 
 }
