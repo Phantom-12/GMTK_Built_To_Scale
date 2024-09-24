3fc3c71a65591b37cf82f4429fd8f5b99606bcbc
Qiangzzz <1105796626@qq.com>
2024-09-25 00:20:31 +0800

    UI适配小优化

--- Assets/Scripts/UISelfAdaption.cs
+++ Assets/Scripts/UISelfAdaption.cs
@@ -12,32 +12,73 @@ public class UISelfAdaption : MonoBehaviour
         RectTransform CanvasRectTransform= GetComponent<RectTransform>();
         foreach (RectTransform rectTransform  in transform)
         {
-            if(rectTransform.name != "BackGround" && rectTransform.name != "Adaption")
+            if(rectTransform.name != "BackGround")
             {
                 if (CanvasRectTransform.rect.width < 1920f)
                 {
-                    if (rectTransform.anchoredPosition.x > 0)
+                    if (rectTransform.name == "MidAdaption")
                     {
-                        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x + (rectTransform.rect.width * (rectTransform.localScale.x * (1f -CanvasRectTransform.rect.width / 1920f))) /2f, rectTransform.anchoredPosition.y);
-                        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x + (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        foreach (RectTransform childRectTransform in rectTransform)
+                        {
+                            if (childRectTransform.anchoredPosition.x > 0)
+                            {
+                                childRectTransform.anchoredPosition = new Vector2(childRectTransform.anchoredPosition.x + (childRectTransform.rect.width * (childRectTransform.localScale.x * (1f - CanvasRectTransform.rect.width / 1920f))) / 2f, childRectTransform.anchoredPosition.y);
+                                childRectTransform.anchoredPosition = new Vector2(childRectTransform.anchoredPosition.x + MathF.Abs(childRectTransform.anchoredPosition.x + childRectTransform.rect.width / 2f) / (CanvasRectTransform.rect.width - 1920f) / 2f, childRectTransform.anchoredPosition.y);
+                            }
+                            else
+                            {
+                                childRectTransform.anchoredPosition = new Vector2(childRectTransform.anchoredPosition.x - (childRectTransform.rect.width * (childRectTransform.localScale.x * (1f - CanvasRectTransform.rect.width / 1920f))) / 2f, childRectTransform.anchoredPosition.y);
+                                childRectTransform.anchoredPosition = new Vector2(childRectTransform.anchoredPosition.x - MathF.Abs(childRectTransform.anchoredPosition.x - childRectTransform.rect.width / 2f) / (CanvasRectTransform.rect.width - 1920f) / 2f, childRectTransform.anchoredPosition.y);
+                            }
+                        }
                     }
                     else
                     {
-                        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x - (rectTransform.rect.width * (rectTransform.localScale.x * (1f - CanvasRectTransform.rect.width / 1920f))) /2f, rectTransform.anchoredPosition.y);
-                        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x - (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        if (rectTransform.anchoredPosition.x > 0)
+                        {
+                            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x + (rectTransform.rect.width * (rectTransform.localScale.x * (1f - CanvasRectTransform.rect.width / 1920f))) / 2f, rectTransform.anchoredPosition.y);
+                            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x + (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        }
+                        else
+                        {
+                            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x - (rectTransform.rect.width * (rectTransform.localScale.x * (1f - CanvasRectTransform.rect.width / 1920f))) / 2f, rectTransform.anchoredPosition.y);
+                            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x - (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        }
                     }
                     rectTransform.localScale = new Vector2(rectTransform.localScale.x * (CanvasRectTransform.rect.width / 1920f), rectTransform.localScale.y * (CanvasRectTransform.rect.width / 1920f));
                 }
-                else
+                else if (Screen.width == 1920)
                 {
-                    if (rectTransform.anchoredPosition.x > 0)
+                    return;
+                }
+                else if (CanvasRectTransform.rect.width > 1920f)
+                {
+                    if(rectTransform.name == "MidAdaption")
                     {
-                        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x + (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        foreach (RectTransform childRectTransform in rectTransform)
+                        {
+                            if (childRectTransform.anchoredPosition.x > 0)
+                            {
+                                childRectTransform.anchoredPosition = new Vector2(childRectTransform.anchoredPosition.x + MathF.Abs(childRectTransform.anchoredPosition.x + childRectTransform.rect.width / 2f) / (CanvasRectTransform.rect.width - 1920f) / 2f, childRectTransform.anchoredPosition.y);
+                            }
+                            else
+                            {
+                                childRectTransform.anchoredPosition = new Vector2(childRectTransform.anchoredPosition.x - MathF.Abs(childRectTransform.anchoredPosition.x - childRectTransform.rect.width / 2f) / (CanvasRectTransform.rect.width - 1920f) / 2f, childRectTransform.anchoredPosition.y);
+                            }
+                        }
                     }
                     else
                     {
-                        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x - (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        if (rectTransform.anchoredPosition.x > 0)
+                        {
+                            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x + (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        }
+                        else
+                        {
+                            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x - (CanvasRectTransform.rect.width - 1920f) / 2f, rectTransform.anchoredPosition.y);
+                        }
                     }
+
                 }
             }
 
